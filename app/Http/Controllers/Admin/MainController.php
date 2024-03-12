5aee5c34b4660fb0a97974347678eb0c696181d7
Unaveramor <p.k.d.04@mail.ru>
2024-03-12 14:31:54 +0500

    fifth commit

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function index(){
-        return view('admin.index');
+        // $tag = new Tag();
+        // $tag->title = 'Привет, очередная проверка, когда ты уже закончишься';
+        // $tag->save();
+
+        return view('admin.index', compact('tag'));
     }
 }
